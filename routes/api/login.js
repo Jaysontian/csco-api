445e19602b1ec59d869ae35b9c7e776f57253e2a
Rishabh Sharma <rishabhsharma7676@gmail.com>
2023-11-23 15:03:08 -0800

    Updated login.js
    Added separate backend functionality for login and signup requests from frontend

--- routes/api/login.js
+++ routes/api/login.js
@@ -4,15 +4,6 @@ const LoginInfo = require('../../models/LoginSchema');
 const Photo = require('../../models/LoginSchema'); // Import the model
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const logins = await LoginInfo.find();
-    res.json(logins);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
 router.post('/', async (req, res) => {
   const newLogin = new LoginInfo({
     username: req.body.username,
@@ -21,22 +12,36 @@ router.post('/', async (req, res) => {
 
   try {
     // perform check for whether user exists or not
-    const user = await LoginInfo.findOne({username: req.body.username });
-    console.log(user)
-    if (user) {
-      if (user.password == req.body.password){
-        console.log("Existing user logged in successfully.")
-        return res.status(200).send({ message: "User logged in successfully"});
-      }
-      return res.status(400).send({ message: "Existing user: password incorrect" });
-    }
+    const user = await LoginInfo.findOne({username: req.body.username });    
+    const login_type = req.body.login_type;    
 
-    // Username DNE, so we create a new account
-    const savedLogin = await newLogin.save();
-    res.status(201).json(savedLogin);
+    // Login Attempt
+    if (login_type == "LOGIN") {
+      if (user) {
+        if (user.password == req.body.password){
+          return res.status(200).json({ message: "User logged in successfully", data: newLogin});
+        }
+        res.status(400)
+        res.statusMessage = "Password incorrect"
+        return res.json({ message: "Password incorrect" });
+      }
+      res.status(400)
+      res.statusMessage = "Username doesn't exist"
+      return res.json({ message: "Username doesn't exist" });
+    } 
+    // Signup attempt
+    else {
+      if (!user) {
+        const savedLogin = await newLogin.save();
+        return res.status(200).json({ message: "User signed up successfully", data: newLogin });
+      }
+      res.status(400)
+      res.statusMessage = "Username already exists"
+      return res.json({ message: "Username already exists"});
+    }    
 
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    return res.status(400).json({ message: err.message });
   }
 });
 
